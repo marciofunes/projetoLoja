164ed5dce4ecab73e00f10435c54fadaefca46e1
marciofunes <marciofunes@gmail.com>
2020-10-22 22:26:03 -0300

    Projeto com Insert e Select no banco

---
+++ src/ClassePrincipal/Classeprincipal.java
@@ -0,0 +1,19 @@
+
+package ClassePrincipal;
+
+import View.ViewPrincipal;
+import conexoes.MySQL;
+
+public class Classeprincipal {
+    public static void main(String[] args) {
+        ViewPrincipal menu = new ViewPrincipal();
+        menu.setVisible(true);
+                
+        //MySQL mysql = new MySQL();
+        //mysql.conectaBanco();
+
+        //menu.setExtendedState(menu.MAXIMIZED_BOTH); 
+        
+    }
+    
+}
